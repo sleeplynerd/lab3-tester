16125bb9658c1570ce8b51ee6213b6fbe0fb9a99
sleeplynerd <dmitry.murzon@gmail.com>
2017-06-27 20:22:57 +0400

    Names correction

--- variant.cpp
+++ variant.cpp
@@ -2,24 +2,19 @@
 
 Variant::Stage( std::string arcsVal, int stageVal) : arcs( arcsVal ), stage( stageVal ) {}
 
-Variant::Variant( int variant ) : vrnt( variant ) {
-	std::string buf;
-	std::string stgListPath( stgListDir + "/" + stgListPfx + stgListPfx + std::to_string( vrnt ) );
-	std::fstream file( stgListPath );
-<<<<<<< HEAD
+bool Variant::isFull() {
+	std::string result = VARIANT( VRNT );
+}
 
-=======
-	
->>>>>>> c5ceea119f1366db29df820be79a5a04ae043696
+Variant::Variant( int variant ) : VRNT( variant ) {
+	std::string buf;
+	std::string stgListPath( STG_LIST_DIR + "/" + STG_LIST_PFX + std::to_string( VRNT ) );
+	std::fstream file( stgListPath );
+
 	if( file ) {
-		std::getline( file, alphabet );						// Первая строка - список дуг
+		std::getline( file, mAlphabet );						// Первая строка - список дуг
 		for( int i = 0; std::getline( file, buf ); i++) {
-			stages.push_back( Stage( buf, i ) );			// Заполняем список очередей
+			mStages.push_back( Stage( buf, i ) );			// Заполняем список очередей
 		}
 	}
 }
-<<<<<<< HEAD
-
-// Hello there 
-=======
->>>>>>> c5ceea119f1366db29df820be79a5a04ae043696
