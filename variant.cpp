6ea56fb09c87fcaf198572df7a05eb1035900bd5
sleeplynerd <dmitry.murzon@gmail.com>
2017-06-27 20:45:49 +0400

    Update variant.cpp

--- variant.cpp
+++ variant.cpp
@@ -10,11 +10,7 @@ Variant::Variant( int variant ) : VRNT( variant ) {
 	std::string buf;
 	std::string stgListPath( STG_LIST_DIR + "/" + STG_LIST_PFX + std::to_string( VRNT ) );
 	std::fstream file( stgListPath );
-<<<<<<< HEAD
 
-=======
->>>>>>> 4c5acb4b365719a631a62525b860d943e7b02079
-	if( file ) {
 		std::getline( file, mAlphabet );						// Первая строка - список дуг
 		for( int i = 0; std::getline( file, buf ); i++) {
 			mStages.push_back( Stage( buf, i ) );			// Заполняем список очередей
