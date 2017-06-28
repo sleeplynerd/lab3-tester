cf7e2d529319cabc9f4a551ba3cbaffdbe1351b7
sleeplynerd <dmitry.murzon@gmail.com>
2017-06-28 18:44:14 +0400

    -

--- variant.cpp
+++ variant.cpp
@@ -1,19 +1,59 @@
 #include "variant.h"
 
-Variant::Stage( std::string arcsVal, int stageVal) : arcs( arcsVal ), stage( stageVal ) {}
-
-bool Variant::isFull() {
-	std::string result = VARIANT( VRNT );
-}
-
 Variant::Variant( int variant ) : VRNT( variant ) {
 	std::string buf;
 	std::string stgListPath( STG_LIST_DIR + "/" + STG_LIST_PFX + std::to_string( VRNT ) );
 	std::fstream file( stgListPath );
 
-		std::getline( file, mAlphabet );						// Первая строка - список дуг
-		for( int i = 0; std::getline( file, buf ); i++) {
-			mStages.push_back( Stage( buf, i ) );			// Заполняем список очередей
+	std::getline( file, mAlphabet );					// Первая строка - список дуг
+	for( int i = 0; std::getline( file, buf ); i++) {
+		mStages.push_back( Stage( buf, i ) );			// Заполняем список очередей
+	}
+}
+
+Variant::Stage( std::string arcsVal, int stageVal) : arcs( arcsVal ), stage( stageVal ) {}
+
+bool Variant::isOrdered() {
+	std::string buf( VARIANT( VRNT ) );
+	bool flag = true;
+	int currStage = 0;
+
+	for( int i = 0; i < buf.length(); i++ ) {
+		if( isEstStage( buf[i], currStage ) );				// Символ относится к текущей очереди
+		else if( isEstStage( buf[i], currStage + 1 ) ) {	// Символ относится к следующей очереди
+			currStage++;
+		} else {
+			flag = false;									// Порядок очередей нарушен
 		}
 	}
+
+	return flag;
+}
+
+bool Variant::isRandom() {
+	const int N_LAUNCHES = 100;
+	std::string	buf = VARIANT( VRNT );
+	bool flag = false;
+
+	for( int i = 0; i < N_LAUNCHES; i++ ) {
+		if( buf.compare( VARIANT( VRNT ) ) == 0 ) {
+			flag = true;
+			i = N_LAUNCHES;
+		}
+	}
+
+	return flag;
+}
+
+bool Variant::isFull() {
+	std::string result = VARIANT( VRNT );
+	bool flag = true;
+
+	for( int i = 0; i < mAlphabet.length(); i++ ) {
+		if( result.find( mAlphabet[i] ) ==  std::string::npos) {
+			flag = false;
+		}
+	}
+
+	return flag;
 }
