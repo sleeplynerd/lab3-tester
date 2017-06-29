477b2056592a0b127cb25467876f60f9d1d7c3c1
sleeplynerd <dmitry.murzon@gmail.com>
2017-06-29 19:44:17 +0400

    -

--- variant.cpp
+++ variant.cpp
@@ -1,3 +1,9 @@
+/*!
+   \file "variant.cpp"
+   \author "Dmitry Murashov"
+   \date "2017-06-28"
+*/
+
 #include "variant.h"
 
 Variant::Variant( int variant ) : VRNT( variant ) {
@@ -11,7 +17,7 @@ Variant::Variant( int variant ) : VRNT( variant ) {
 	}
 }
 
-Variant::Stage( std::string arcsVal, int stageVal) : arcs( arcsVal ), stage( stageVal ) {}
+Variant::Stage( std::string stageAlphabet, int stageVal) : stageAlphabet( arcsVal ), stage( stageVal ) {}
 
 bool Variant::isOrdered() {
 	std::string buf( VARIANT( VRNT ) );
@@ -47,7 +53,7 @@ bool Variant::isRandom() {
 }
 
 bool Variant::isFull() {
-	std::string result = VARIANT( VRNT );
+	std::string result( VARIANT( VRNT ) );
 	bool flag = true;
 
 	for( int i = 0; i < mAlphabet.length(); i++ ) {
@@ -59,10 +65,62 @@ bool Variant::isFull() {
 	return flag;
 }
 
+bool Variant::isParallel() {
+	const std::string output( VARIANT( VRNT ) );
+	std::list<Stage>::iterator it;
+	bool flag = true;
+
+	for( it = mStages.begin(); it != mStages.end(); ++it ) {
+		flag = flag && isLongStage( it, output ) && isParallelStage( it, output );
+		if( !flag ) {
+			it = mStages.end();
+		}
+	}
+
+	return flag;
+}
+
+bool Variant::isLongStage( const Stage& stage, const std::string& output ) {
+	return ( getStageFragment( stage, output ) > stage -> stageAlphabet );
+}
+
+bool Variant::isParallelStage( const Stage& stage, const std::string& output ) {
+	const std::string sf( getStageFragment( stage, output ) );	// Stage Fragment
+	const std::string sa( stage -> stageAlphabet );				// Stage Alphabet
+	int sfp = 0;												// Stage Fragment Position
+	int sap = 0;												// Stage Alphabet Position
+	int counter;
+	bool flag = false;
+
+	for( sap = 0; sap < sa.length(); ++sap ) {					// Обход каждого элемента "алфавита"
+		counter = std::count( sf.begin(), sf.end(), sa[sap] );
+		// Если во фрагменте больше одного вхождения элемента, становятся
+		//   возможными их (элементов) взаимные перестановки.
+		//   Проверяем, является ли их появление случайным или
+		//   последовательным (AAABBB и ABBABBAA).
+		if( counter >= 2 ) {
+			// Пробегаем по фрагменту начиная с первого
+			//   вхождения элемента алфавита
+			sfp = sf.find( sa[sap] );
+			for( int i = sfp + 1; i < sfp + counter; ++i) {
+				// Если на следующих после первого вхождения позициях
+				//   появление искомого элемента прерывается хотя
+				//   бы одним отличным символом, признаём фрагмент корректным.
+				if( sf[i] != sf[sfp] ) {
+					flag = true;
+					// Выход из циклов
+					sap = sa.length();
+					i = sfp + counter;
+				}
+			}
+		}
+	}
+}
+
 bool Variant::isEstStage( char symbol, int stage ) {
 	bool flag = false;
 
-	for( std::list<Stage>::iterator it = mStages.begin(); it!= mStages.end(); ++it ) {
+	for( std::list<Stage>::iterator it = mStages.begin(); it != mStages.end(); ++it ) {
 		if( it -> stage == stage ) {
 			if( it -> arcs.find( symbol )  != std::string::npos ) {
 				flag = true;
@@ -70,4 +128,21 @@ bool Variant::isEstStage( char symbol, int stage ) {
 			break;
 		}
 	}
+}
+
+std::string Variant::getStageFragment( const Stage& stage, const std::string& output ) {
+	std::string buf( "" );
+	int i = 0;
+
+	// Ищем позицию начала очереди
+	while( !isEstStage( output[i], stage -> stage ) && ( i < output.length() ) ) {
+		i++;
+	}
+	// Копируем элементы, принадлежащие данной очереди
+	while( isEstStage( output[i], stage -> stage ) && ( i < output.length() ) ) {
+		buf.append( output[i] );
+		i++;
+	}
+
+	return buf;
 }
