c82969927f0658ea18432b23aa75501c41312117
sleeplynerd <dmitry.murzon@gmail.com>
2017-07-06 19:39:02 +0400

    - main() in VARIANT_1.cpp

--- VARIANTS/VARIANT_1.cpp
+++ VARIANTS/VARIANT_1.cpp
@@ -1,5 +1,17 @@
 #include <string>
 
+/**
+ * Множество кода
+ */
+
+// TODO: удалить это
+int main() {
+	/**
+	 * Множество кода
+	 */
+	return 0;
+}
+
 std::string variant1() {
-	return "acfbedkghmnp";
+	return "aaacfdbdefbbdbkgmmhkmmbnbnpppp";
 }
