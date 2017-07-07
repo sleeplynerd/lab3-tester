868007103d33fc9f92f83cf5897de48590799b47
sleeplynerd <dmitry.murzon@gmail.com>
2017-07-08 01:19:43 +0400

    again

--- test/main.cpp
+++ test/main.cpp
@@ -5,85 +5,85 @@
 // #define IOSTREAM_H
 // #include <iostream>
 // #endif /* end of include guard: IOSTREAM_H */
-TEST( VARIANT_1,  all ) { 
+TEST( VARIANT_1,  all ) {
     Variant v( 1 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_2,  all ) {
     Variant v( 2 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_3,  all ) {
     Variant v( 3 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_4,  all ) {
     Variant v( 4 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_5,  all ) {
     Variant v( 5 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_6,  all ) {
     Variant v( 6 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_7,  all ) {
     Variant v( 7 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_8,  all ) {
     Variant v( 8 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_9,  all ) {
     Variant v( 9 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_10,  all ) {
     Variant v( 10 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_11,  all ) {
     Variant v( 11 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_12,  all ) {
     Variant v( 12 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_13,  all ) {
     Variant v( 13 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_14,  all ) {
     Variant v( 14 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_15,  all ) {
     Variant v( 15 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_16,  all ) {
     Variant v( 16 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_17,  all ) {
     Variant v( 17 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_18,  all ) {
     Variant v( 18 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_19,  all ) {
     Variant v( 19 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 TEST( VARIANT_20,  all ) {
     Variant v( 20 );
-    EXPECT_TRUE( v.isOrdered() && isRandom() && isParallel() && isFull() );
+    EXPECT_TRUE( v.isOrdered() && v.isRandom() && v.isParallel() && v.isFull() );
 }
 
 
